a9fb45c7953f7b4546c9e7e9b73810b76c5b6911
chestnutme <1724759858@qq.com>
2016-05-07 22:49:29 +0800

    add ConfigParser in config.py

--- config.py
+++ config.py
@@ -1,18 +1,22 @@
 import os
+from ConfigParser import ConfigParser
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    conf = ConfigParser()
+    conf.read('config.ini')
+    SECRET_KEY = conf.get('key', 'SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_SERVER = conf.get('email', 'MAIL_SERVER')
+    MAIL_PORT = conf.get('email', 'MAIL_PORT')
+    MAIL_USE_TLS = bool(int(conf.get('email', 'MAIL_USE_TLS')))
+    MAIL_USE_SSL = bool(int(conf.get('email', 'MAIL_USE_SSL')))
+    MAIL_USERNAME = conf.get('email', 'MAIL_USERNAME')
+    MAIL_PASSWORD = conf.get('email', 'MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = conf.get('email', 'FLASKY_MAIL_SUBJECT_PREFIX')
+    FLASKY_MAIL_SENDER = conf.get('email', 'FLASKY_MAIL_SENDER')
+    FLASKY_ADMIN = conf.get('email', 'FLASKY_ADMIN')
 
     @staticmethod
     def init_app(app):
