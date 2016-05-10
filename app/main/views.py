7ecc9c0632bb9f79b7b143f5944e1083f3fdabde
chestnutme <1724759858@qq.com>
2016-05-10 16:12:10 +0800

    add permanent link to posts

--- app/main/views.py
+++ app/main/views.py
@@ -26,7 +26,7 @@ def index():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'], error_out=False)
     posts = pagination.items
     return render_template('user.html', user=user, posts = posts, pagination=pagination)
@@ -72,3 +72,8 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+    
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
